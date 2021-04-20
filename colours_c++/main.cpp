9630199c41e0768553a2bf002339495a13c13237
Charlieash <charlieashdown15@gmail.com>
2021-04-20 10:31:06 +0100

    Update main.cpp

--- colours_c++/main.cpp
+++ colours_c++/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <stdio.h>
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
-using namespace std;
+using namespace cv;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    namedWindow("HSV Tester");
+    int lowH = 0, highH = 179, lowS = 0, highS = 255, lowV = 0, highV = 255;
+
+    createTrackbar("Low Hue", "HSV Tester", &lowH,179,NULL);
+    createTrackbar("High Hue", "HSV Tester", &highH,179,NULL);
+
+    createTrackbar("Low Sat", "HSV Tester", &lowS,255,NULL);
+    createTrackbar("High Sat", "HSV Tester", &highS,255,NULL);
+
+    createTrackbar("Low Value", "HSV Tester", &lowV,255,NULL);
+    createTrackbar("High Value", "HSV Tester", &highV,255,NULL);
+
+    Mat frame = imread("OpenCV_Logo.png");
 }
