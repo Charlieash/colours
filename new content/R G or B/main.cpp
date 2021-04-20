a991cddd0f405e830196703276b68be8bb1c2a61
Charlieash <charlieashdown15@gmail.com>
2021-04-20 14:42:05 +0100

    finished r g or b program
    This program allows the user to select either R G or B and the image will display the section of the chosen colour converted to white and black

--- new content/R G or B/main.cpp
+++ new content/R G or B/main.cpp
@@ -11,34 +11,46 @@ The raw camera feed is shown along with the inRange result. Silders allow the HS
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include <string>
 
 using namespace cv;
 
 int main( )
 {
-    namedWindow("HSV Tester");   // Create a GUI window called photo
-
+    //namedWindow("HSV Tester");   // Create a GUI window called photo
+    std::string colour = "ble";
     int lowH = 0, highH = 179, lowS = 0, highS = 255, lowV = 0, highV = 255;    // Initialise some variables for HSV limits
 
-    createTrackbar("Low Hue", "HSV Tester", &lowH, 179, NULL);      // Create trackbar controls for each HSV limit
-    createTrackbar("High Hue", "HSV Tester", &highH, 179, NULL);
+    //createTrackbar("Low Hue", "HSV Tester", &lowH, 179, NULL);      // Create trackbar controls for each HSV limit
+    //createTrackbar("High Hue", "HSV Tester", &highH, 179, NULL);
 
-    createTrackbar("Low Sat", "HSV Tester", &lowS, 255, NULL);
-    createTrackbar("High Sat", "HSV Tester", &highS, 255, NULL);
+    //createTrackbar("Low Sat", "HSV Tester", &lowS, 255, NULL);
+    //createTrackbar("High Sat", "HSV Tester", &highS, 255, NULL);
 
-    createTrackbar("Low Value", "HSV Tester", &lowV, 255, NULL);
-    createTrackbar("High Value", "HSV Tester", &highV, 255, NULL);
+    //createTrackbar("Low Value", "HSV Tester", &lowV, 255, NULL);
+    //createTrackbar("High Value", "HSV Tester", &highV, 255, NULL);
 
     Mat frame = imread("OpenCV_Logo.png"); // Open an image file and store in a new matrix variable
+    std::cout << "R, G or B? ";
+    std::cin >> colour;
+    if(colour == "B"){
+        lowH = 120, highH = 179, lowS = 0, highS = 255, lowV = 0, highV = 255;
+    }
+    else if(colour == "G"){
+        lowH = 35, highH = 80, lowS = 0, highS = 255, lowV = 0, highV = 255;
+    }
+    else if(colour == "R"){
+        lowH = 0, highH = 0, lowS = 255, highS = 255, lowV = 0, highV = 255;
+    };
 
     while(1)    // Main loop to perform image processing
     {
-        lowH = getTrackbarPos("Low Hue", "HSV Tester");        // Update the variables with the trackbar setting
-        highH = getTrackbarPos("High Hue", "HSV Tester");
-        lowS = getTrackbarPos("Low Sat", "HSV Tester");
-        highS = getTrackbarPos("High Sat", "HSV Tester");
-        lowV = getTrackbarPos("Low Value", "HSV Tester");
-        highV = getTrackbarPos("High Value", "HSV Tester");
+        //lowH = getTrackbarPos("Low Hue", "HSV Tester");        // Update the variables with the trackbar setting
+        //highH = getTrackbarPos("High Hue", "HSV Tester");
+        //lowS = getTrackbarPos("Low Sat", "HSV Tester");
+        //highS = getTrackbarPos("High Sat", "HSV Tester");
+        //lowV = getTrackbarPos("Low Value", "HSV Tester");
+        //highV = getTrackbarPos("High Value", "HSV Tester");
 
         Mat frameHSV;       // Convert the frame to HSV and apply the limits
         cvtColor(frame, frameHSV, COLOR_BGR2HSV);
@@ -48,7 +60,7 @@ int main( )
         cvtColor(frameHSV, frameHSV, COLOR_GRAY2BGR);   // In range returns the equivalent of a grayscale image so we need to convert this before concatenation
         hconcat(frame, frameHSV, comparison);
 
-        imshow("HSV Tester", comparison); //Display the image in the window
+        imshow("Converted image", frameHSV); //Display the image in the window
 
         int key = cv::waitKey(1);   // Wait 1ms for a keypress (required to update windows
 
