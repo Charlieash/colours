51fd30a49ae8a31663997a0c850709b1c3022426
Charlieash <charlieashdown15@gmail.com>
2021-04-20 10:56:55 +0100

    finished example

--- colours_c++/main.cpp
+++ colours_c++/main.cpp
@@ -21,4 +21,29 @@ int main()
     createTrackbar("High Value", "HSV Tester", &highV,255,NULL);
 
     Mat frame = imread("OpenCV_Logo.png");
+
+    while(1)
+    {
+        lowH = getTrackbarPos("Low Hue", "HSV Tester");
+        highH = getTrackbarPos("High Hue", "HSV Tester");
+        lowS = getTrackbarPos("Low Sat", "HSV Tester");
+        highS = getTrackbarPos("High Sat", "HSV Tester");
+        lowV = getTrackbarPos("Low Value", "HSV Tester");
+        highV = getTrackbarPos("High Value", "HSV Tester");
+
+        Mat frameHSV;
+        cvtColor(frame, frameHSV, COLOUR_BGR2HSV);
+        inRange(frameHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), frameHSV);
+        Mat comparison;
+        cvtColor(frameHSV, frameHSV, COLOR_GRAY2BGR);
+        hconcat(frame, frameHSV, comparison);
+        imshow("HSV Tester", comparison);
+
+        int key = cv::waitkey(1);
+        key = (key ==255) ? 1 :key;
+        if(key == 27)
+            break;
+    }
+    destroyAllWindows();
+    return 0;
 }
